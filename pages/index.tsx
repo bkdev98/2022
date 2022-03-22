b64180b20044688dad5fc710a82f986564cb550c
Quoc Khanh <bkdev98@gmail.com>
2022-03-22 09:45:15 +0700

    Config linting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,35 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import type { NextPage } from 'next'
+import Head from 'next/head'
 
-const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Quoc Khanh</title>
-        <meta
-          name="description"
-          content="Quoc Khanh's personal site & portfolio"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+import styles from '../styles/Home.module.css'
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Everything</h1>
+const Home: NextPage = () => (
+  <div className={styles.container}>
+    <Head>
+      <title>Quoc Khanh</title>
+      <meta
+        name="description"
+        content="Quoc Khanh's personal site & portfolio"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-        <p className={styles.description}>is more complicated than you think</p>
-      </main>
+    <main className={styles.main}>
+      <h1 className={styles.title}>Everything</h1>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://buiquockhanh.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          The personal site & portfolio of HCMC-based developer Quoc Khanh
-        </a>
-      </footer>
-    </div>
-  );
-};
+      <p className={styles.description}>is more complicated than you think</p>
+    </main>
 
-export default Home;
+    <footer className={styles.footer}>
+      <a
+        href="https://buiquockhanh.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        The personal site & portfolio of HCMC-based developer Quoc Khanh
+      </a>
+    </footer>
+  </div>
+)
+
+export default Home
