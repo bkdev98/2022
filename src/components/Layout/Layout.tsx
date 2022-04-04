7455f2e69ff271a4fdebe08878a2b0f1d69ff81f
Quoc Khanh <bkdev98@gmail.com>
2022-04-04 14:09:25 +0700

    Add mobile banner

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,32 @@
 import * as React from 'react'
 
 import { motion } from 'framer-motion'
+import styled from 'styled-components'
+
+const ShowOnDesktop = styled.div`
+  display: none;
+  @media (min-width: 1000px) {
+    display: block;
+  }
+`
+
+const ShowOnMobile = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  max-width: ${({ theme }) => theme.sizes.container}px;
+  margin: 0px auto;
+  @media (min-width: 1000px) {
+    display: none;
+  }
+  h1 {
+    font-family: 'Silk Serif', serif;
+    font-weight: 100;
+    text-align: center;
+    margin: 20px;
+  }
+`
 
 const Layout: React.FC = ({ children }) => (
   <motion.main
@@ -14,7 +40,13 @@ const Layout: React.FC = ({ children }) => (
     exit="exit"
     transition={{ type: 'linear' }}
   >
-    {children}
+    <ShowOnDesktop>{children}</ShowOnDesktop>
+    <ShowOnMobile>
+      <h1>
+        The mobile version is currently in progress, please access this site
+        from a desktop.
+      </h1>
+    </ShowOnMobile>
   </motion.main>
 )
 
